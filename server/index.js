89932e0b895ba70da5b8d8bffc467aaece4e4b8d
KCmean <kunalmanu2468@gmail.com>
2023-06-03 22:55:37 +0530

    feat: sockets connections

--- server/index.js
+++ server/index.js
@@ -8,7 +8,27 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+// const io = socketio(server);
+// const io = require("socket.io")(server, { 
+//     cors: {
+//       origin: "localhost:3000",
+//       credentials: true
+//     }
+//   });
+
+// const io = require("socket.io")(httpServer, {
+//     allowRequest: (req, callback) => {54n l;'
+
+//       const noOriginHeader = req.headers.origin === undefined;
+//       callback(null, noOriginHeader);
+//     }
+//   });
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "localhost:3000",
+    }
+  });
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
