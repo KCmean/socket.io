240ef17ea087779f638b7c9c9547694fd3a87452
KCmean <kunalmanu2468@gmail.com>
2023-06-06 19:34:48 +0530

    feat: server emit events

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {  addUser, removeUser, getUser, getUsersInRoom  } = require('./users.js')
+
 const PORT  = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -10,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 // const io = socketio(server);
 // const io = require("socket.io")(server, { 
-//     cors: {
+//     cors: { 
 //       origin: "localhost:3000",
 //       credentials: true
 //     }
@@ -32,6 +34,20 @@ const io = require("socket.io")(server, {
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
+
+    socket.on('join', ({name,room} , callback) => {
+      console.log(name,room);
+
+      const {error , user } = addUser({ id: socket.id, name, room});
+
+      if(error){
+       return callback(error);
+      }
+
+      socket.emit('message' , {user: 'admin' })
+
+      socket.join(user.room);
+    });
 
     socket.on('disconnect', () => {
         console.log('User had left!!! :(');
