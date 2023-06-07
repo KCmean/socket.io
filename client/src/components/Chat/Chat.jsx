08a532ccc6950c418c22f5446a5f56af047d972f
KCmean <kunalmanu2468@gmail.com>
2023-06-07 13:57:31 +0530

    feat: send message functionality

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css'
 
 
 const Chat = () => {
   const location = useLocation();
   const [name, setName] = useState("");
-  const [room, setRoom]  = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
   const ENDPOINT = 'localhost:5000';
 
   // console.log(location)
@@ -15,25 +19,14 @@ const Chat = () => {
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
-
-    // const socket = io(ENDPOINT);
-
-    // const socket = io(ENDPOINT, {
-    //   transports: ['websocket', 'polling', 'flashsocket']
-    // });
     const socket = io(ENDPOINT, {
       transports: ['websocket']
-    },  )
-
-    // socket.on('fromServer', (data) => {
-    //   setName(name);
-    //   setRoom(room);
-    // });
+    },)
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name , room }, ({ error }) => {
-      alert(error); 
+    socket.emit('join', { name, room }, ({ error }) => {
+      alert(error);
     });
 
     console.log(socket);
@@ -41,13 +34,46 @@ const Chat = () => {
     return () => {
       socket.emit('disconnect')
 
-        socket.off();
+      socket.off();
     }
-  }, [ENDPOINT, location.search])
+  }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    const socket = io(ENDPOINT, {
+      transports: ['websocket']
+    },)
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+
+    }, [messages])
+  })
+
+
+  //function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+    const socket = io(ENDPOINT, {
+      transports: ['websocket']
+    },)
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  console.log(message, messages);
 
   return (
-    <div>Chat</div>
+    <div className='outerContainer'>
+      <div className='container'>
+        <input
+          value={message}
+          type="text"
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyDown={(event) => event.key === 'Enter' ? sendMessage(event) : null}
+
+        />
+      </div>
+    </div>
   )
 }
 
